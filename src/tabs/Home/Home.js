33bbf946d3898f3bbc0f19b9fb15579cddd9a395
Michael Stork <mstork@stream9.net>
2017-10-04 17:53:28 -0600

    news

--- src/tabs/Home/Home.js
+++ src/tabs/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Panel from '../../components/Panel/Panel';
 import axios from 'axios';
 import './Home.css';
+import Moment from 'react-moment';
 
 class Home extends React.Component {
 	constructor(props) {
@@ -21,8 +22,19 @@ class Home extends React.Component {
 
 	render() {
 		return (
-			<Panel>
+			<Panel className="news-panel">
 				<h2>News &amp; Information</h2>
+				<div>
+					{this.state.news.map(item =>
+						<div className="news-item" key={item.news_id}>
+							<h4>{item.news_title}</h4>
+							<p className="news-item-author">
+								{item.user.user_email} on <Moment format="dddd\, MMM Do \a\t h:mm A">{item.news_created}</Moment>
+							</p>
+							<p className="news-item-content" dangerouslySetInnerHTML={{__html: item.news_content}} />
+						</div>
+					)}
+				</div>
 			</Panel>
 		);
 	}
