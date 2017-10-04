f72057b77655efd0e867c5d816b5e5272fae9701
Michael Stork <mstork@stream9.net>
2017-10-04 10:53:30 -0600

    fetch test

--- src/tabs/Home/Home.js
+++ src/tabs/Home/Home.js
@@ -2,10 +2,30 @@ import React from 'react';
 import Panel from '../../components/Panel/Panel';
 import './Home.css';
 
-const Home = (props) => (
-	<Panel>
-		<h2>Home</h2>
-	</Panel>
-)
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			news: []
+		};
+	}
+
+	componentDidMount() {
+		fetch('http://mspsych.localhost/api/news').then(data => {
+			console.log(data.json);
+			this.setState({
+				news: data.json
+			});
+		});
+	}
+
+	render() {
+		return (
+			<Panel>
+				<h2>News &amp; Information</h2>
+			</Panel>
+		);
+	}
+}
 
 export default Home;
