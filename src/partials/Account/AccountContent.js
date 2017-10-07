714c39255733714ba0bddedf222ec8829bfa3741
Michael Stork <mstork@stream9.net>
2017-10-07 16:23:57 -0600

    simplify account components

--- src/partials/Account/AccountContent.js
+++ src/partials/Account/AccountContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import AccountUpdateContainer from '../../containers/AccountUpdateContainer';
+import AccountUpdate from '../../partials/Account/AccountUpdate';
 import AccountHome from '../../partials/Account/AccountHome';
 
 
@@ -8,8 +8,10 @@ const AccountContent = (props) => (
 	<Switch>
 		<Route exact path={props.match.url} render={() =>
 			<AccountHome email={props.email} logout={props.logout} match={props.match} />
-		}/>
-		<Route path={props.match.url + '/update'} component={AccountUpdateContainer} />
+		} />
+		<Route path={props.match.url + '/update'} render={() =>
+			<AccountUpdate logout={props.logout} />
+		} />
 	</Switch>
 )
 
--- src/partials/Account/AccountUpdate.js
+++ src/partials/Account/AccountUpdate.js
@@ -1,32 +1,85 @@
 import React from 'react';
+import axios from '../../connection/axios';
 import {Link} from 'react-router-dom';
 
-const AccountUpdate = (props) => (
-	<div className="account-update">
-		{props.message
-			? <p className="message">{props.message}</p>
-			: ''}
-		<form onSubmit={props.handleSubmit} name="passwordUpdate">
-			<div className="input-container">
-				<label>Current Password</label>
-				<input onChange={props.handleChange} value={props.current} name="current" type="password" />
+class AccountUpdate extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			current: '',
+			password: '',
+			confirm: '',
+			message: null
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		switch (event.target.name) {
+			case 'current':
+				this.setState(Object.assign({}, this.state, {current: event.target.value}));
+				break;
+			case 'password':
+				this.setState(Object.assign({}, this.state, {password: event.target.value}));
+				break;
+			case 'confirm':
+				this.setState(Object.assign({}, this.state, {confirm: event.target.value}));
+				break;
+			default:
+				return;
+		}
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		axios.post(
+			'/api/update-password',
+			{
+				current: this.state.current,
+				password: this.state.password,
+				confirm: this.state.confirm
+			}
+		)
+		.then(response => {
+			this.props.logout(response.data.message);
+		})
+		.catch(error => {
+			this.setState(Object.assign({}, this.state, {message: error.response.data.message}));
+		})
+	}
+
+	render() {
+		return (
+			<div className="account-update">
+				{this.state.message
+					? <p className="message">{this.state.message}</p>
+					: ''}
+				<form onSubmit={this.handleSubmit} name="passwordUpdate">
+					<div className="input-container">
+						<label>Current Password</label>
+						<input onChange={this.handleChange} value={this.state.current} name="current" type="password" />
+					</div>
+					<div className="input-container">
+						<label>New Password</label>
+						<input onChange={this.handleChange} value={this.state.password} name="password" type="password" />
+					</div>
+					<div className="input-container">
+						<label>Confirm Password</label>
+						<input onChange={this.handleChange} value={this.state.confirm} name="confirm" type="password" />
+					</div>
+					<div className="button-container">
+						<Link to='/account'>
+							<button type="button" className="cancel">Cancel</button>
+						</Link>
+						<button type="submit">Submit</button>
+					</div>
+				</form>
 			</div>
-			<div className="input-container">
-				<label>New Password</label>
-				<input onChange={props.handleChange} value={props.password} name="password" type="password" />
-			</div>
-			<div className="input-container">
-				<label>Confirm Password</label>
-				<input onChange={props.handleChange} value={props.confirm} name="confirm" type="password" />
-			</div>
-			<div className="button-container">
-				<Link to='/account'>
-					<button type="button" className="cancel">Cancel</button>
-				</Link>
-				<button type="submit">Submit</button>
-			</div>
-		</form>
-	</div>
-)
+
+		);
+	}
+}
 
 export default AccountUpdate;
