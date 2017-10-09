b3bfbca95ce809598a9dc667286ae039976d160f
Michael Stork <mstork@stream9.net>
2017-10-08 19:00:07 -0600

    delete news styles

--- src/tabs/Admin/News/News.js
+++ src/tabs/Admin/News/News.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './News.css';
 import axios from '../../../connection/axios';
-import Moment from 'react-moment';
 
 class News extends React.Component {
 	constructor(props) {
@@ -26,15 +25,7 @@ class News extends React.Component {
 			<section>
 				<h2>News</h2>
 				<div className="admin-news-content">
-					{this.state.news.map(item =>
-						<div className="news-item" key={item.id}>
-							<h4>{item.title}</h4>
-							<p className="news-item-author">
-								{item.user.email} on <Moment format="dddd\, MMM Do \a\t h:mm A">{item.created_at}</Moment>
-							</p>
-							<p className="news-item-content" dangerouslySetInnerHTML={{__html: item.content}} />
-						</div>
-					)}
+
 				</div>
 			</section>
 		);
--- src/tabs/Home/Home.js
+++ src/tabs/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import CSSTransition from 'react-transition-group/CSSTransition';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+
 import Panel from '../../components/Panel/Panel';
 import axios from '../../connection/axios';
 import './Home.css';
 import Moment from 'react-moment';
+
+
 
 class Home extends React.Component {
 	constructor(props) {
@@ -36,19 +41,21 @@ class Home extends React.Component {
 			<Panel className="news-panel">
 				<h2>News &amp; Information</h2>
 				<div>
-					{this.state.news.map(item =>
-						<div className="news-item" key={item.id}>
-							{this.props.user && this.props.user.isAdmin
-								? <span onClick={() => this.deleteNews(item.id)}>Delete</span>
-								: ''
-							}
-							<h4>{item.title}</h4>
-							<p className="news-item-author">
-								{item.user.email} on <Moment format="dddd\, MMM Do \a\t h:mm A">{item.created_at}</Moment>
-							</p>
-							<p className="news-item-content" dangerouslySetInnerHTML={{__html: item.content}} />
-						</div>
-					)}
+					<TransitionGroup>
+						{this.state.news.map((item) =>
+							<CSSTransition {...this.props} timeout={300} classNames="fade" in={this.state.show} key={item.id}>
+								<div className="news-item">
+									{this.props.user && this.props.user.isAdmin
+										&& <i onClick={() => this.deleteNews(item.id)} className="material-icons">clear</i>}
+									<h4>{item.title}</h4>
+									<p className="news-item-author">
+										{item.user.email} on <Moment format="dddd\, MMM Do \a\t h:mm A">{item.created_at}</Moment>
+									</p>
+									<p className="news-item-content" dangerouslySetInnerHTML={{__html: item.content}} />
+								</div>
+							</CSSTransition>
+						)}
+					</TransitionGroup>
 				</div>
 			</Panel>
 		);
