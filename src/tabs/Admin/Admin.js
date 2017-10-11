09058286f38b83cedd58e047ace469dee8fe2833
Michael Stork <mstork@stream9.net>
2017-10-11 14:50:51 -0600

    rename Photos -> Students

--- src/tabs/Admin/Admin.js
+++ src/tabs/Admin/Admin.js
@@ -7,7 +7,7 @@ import './Admin.css';
 import Overview from './Overview/Overview';
 import Users from './Users/Users';
 import Spreadsheets from './Spreadsheets/Spreadsheets';
-import Photos from './Photos/Photos';
+import Students from './Students/Students';
 
 const Admin = ({match}) => (
 	<div className="admin-panel-container">
@@ -15,14 +15,14 @@ const Admin = ({match}) => (
 			<NavLink activeClassName="active-nav-item" to={match.url + '/overview'}>Evals Overview</NavLink>
 			<NavLink activeClassName="active-nav-item" to={match.url + '/users'}>User Management</NavLink>
 			<NavLink activeClassName="active-nav-item" to={match.url + '/spreadsheets'}>Spreadsheets</NavLink>
-			<NavLink activeClassName="active-nav-item" to={match.url + '/photos'}>Student Photos</NavLink>
+			<NavLink activeClassName="active-nav-item" to={match.url + '/students'}>Students</NavLink>
 		</nav>
 		<Panel className="admin-panel with-items">
 			<Switch>
 				<Route exact path={match.url + '/overview'} component={Overview} />
 				<Route path={match.url + '/users'} component={Users} />
 				<Route path={match.url + '/spreadsheets'} component={Spreadsheets} />
-				<Route path={match.url + '/photos'} component={Photos} />
+				<Route path={match.url + '/students'} component={Students} />
 				<Redirect from={match.url} to={match.url + '/overview'} />
 			</Switch>
 		</Panel>
---
+++ src/tabs/Admin/Students/Students.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Students.css';
+
+const Students = (props) => (
+	<section>
+		<h2>Students</h2>
+	</section>
+)
+
+export default Students;
