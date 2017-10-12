4755e0a1ec804fe1203953b7935586653ef78875
Michael Stork <mstork@stream9.net>
2017-10-11 21:27:25 -0600

    remove spreadsheets, add assignments

---
+++ src/tabs/Admin/Assignments/Assignments.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Assignments.css';
+
+const Assignments = (props) => (
+	<section>	
+		<h2>Assign Evaluations</h2>
+	</section>
+)
+
+export default Assignments;
