c8865cb0bf899496da770b73e4450b813a84db05
Michael Stork <mstork@stream9.net>
2017-10-22 17:14:40 -0600

    response interceptor to check for expired token and show notification

--- src/connection/axios.js
+++ src/connection/axios.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import store from '../store';
+import notificationsActions from '../actions/notifications';
+import authActions from '../actions/auth';
 
 const instance = axios.create();
 
@@ -7,6 +10,27 @@ instance.interceptors.request.use(function (config) {
 	if (token) config.headers.common['Authorization'] = 'Bearer ' +  token;
 	return config;
 }, function (error) {
+	return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response) {
+	return response;
+}, function (error) {
+	if (error.response.status === 401) {
+		store.dispatch(
+			authActions.logout()
+		);
+
+		if (error.response.data.error === 'token_expired') {
+			store.dispatch(
+				notificationsActions.notify(
+					'Your session has expired, please log in again.'
+				)
+			);
+		}
+		
+		window.location.hash = 'account';
+	}
 	return Promise.reject(error);
 });
 
