70168ac0a91fcd2185b782e0d8e8d4724bdcd114
Michael Stork <mstork@stream9.net>
2017-10-31 15:02:27 -0600

    completed evaluations header, loader, and no results message

--- src/tabs/Evaluations/CompletedEvals/CompletedEvals.js
+++ src/tabs/Evaluations/CompletedEvals/CompletedEvals.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './CompletedEvals.css';
 import {Link} from 'react-router-dom'
+import Loader from '../../../components/Loader/Loader';
 import CSSTransition from 'react-transition-group/CSSTransition';
 import axios from '../../../connection/axios';
 import moment from 'moment';
@@ -11,7 +12,8 @@ class CompletedEvals extends React.Component {
 		super(props);
 
 		this.state = {
-			evals: []
+			evals: [],
+			inProgress: true
 		};
 	}
 
@@ -28,17 +30,65 @@ class CompletedEvals extends React.Component {
 	}
 
 	getUserCompletedEvals() {
+		const state = cloneDeep(this.state);
+		state.inProgress = true;
+		this.setState(state);
+
 		return axios.get(
 			'/api/evaluations/user/'+this.props.user.id+'/completed'
 		)
 		.then(response => {
 			const state = cloneDeep(this.state);
 			state.evals = response.data;
+			state.inProgress = false;
 			this.setState(state);
 		})
 		.catch(error => {
 			console.log(error);
+			const state = cloneDeep(this.state);
+			state.inProgress = false;
+			this.setState(state);
 		});
+	}
+
+	renderCompletedEvals() {
+		if (!this.state.evals.length && !this.state.inProgress) {
+			return (
+				<p className="no-results">You have no completed evaluations</p>
+			);
+		}
+
+		return (
+			<CSSTransition
+                in={this.state.evals.length > 0}
+                classNames="fade"
+                mountOnEnter={true}
+                unmountOnExit={true}
+                timeout={8000}>
+				<div>
+					{this.state.evals.map(evaluation =>
+						<Link className="panel-item completed-eval-panel-item"
+							to={
+								this.props.match.url + '/' + evaluation.id
+							}
+							key={evaluation.id}>
+							<div className="eval-info-container">
+								<p className="student-name">{evaluation.student.name}</p>
+								<p className="eval-type-date">
+									{evaluation.type.name} - {formatDate(evaluation.updated_at)}
+								</p>
+							</div>
+							<div className="average-score-container">
+								<p>Average Score:</p>
+								<div className="counter">
+									{parseFloat(evaluation.average.toFixed(2))}
+								</div>
+							</div>
+						</Link>
+					)}
+				</div>
+			</CSSTransition>
+		);
 	}
 
 	render() {
@@ -49,35 +99,13 @@ class CompletedEvals extends React.Component {
 					<span>My Evaluations</span>
 				</Link>
 
-				<CSSTransition
-	                in={this.state.evals.length > 0}
-	                classNames="fade"
-	                mountOnEnter={true}
-	                unmountOnExit={true}
-	                timeout={350}>
-					<div className="panel-content completed-evals">
-						{this.state.evals.map(evaluation =>
-							<Link className="panel-item completed-eval-panel-item"
-								to={
-									this.props.match.url + '/' + evaluation.id
-								}
-								key={evaluation.id}>
-								<div className="eval-info-container">
-									<p className="student-name">{evaluation.student.name}</p>
-									<p className="eval-type-date">
-										{evaluation.type.name} - {formatDate(evaluation.updated_at)}
-									</p>
-								</div>
-								<div className="average-score-container">
-									<p>Average Score:</p>
-									<div className="counter">
-										{parseFloat(evaluation.average.toFixed(2))}
-									</div>
-								</div>
-							</Link>
-						)}
-					</div>
-				</CSSTransition>
+				<div className="panel-content completed-evals">
+					<h2>
+						<span>Your Completed Evaluations</span>
+						<Loader loading={this.state.inProgress} />
+					</h2>
+					{this.renderCompletedEvals()}
+				</div>
 			</section>
 		);
 	}
